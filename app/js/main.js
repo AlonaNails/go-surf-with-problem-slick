941d6c0acfa8bed73a54db2432fcca582c88429a
unknown <reuvenslepnev@gmail.com>
2020-05-27 11:39:42 +0300

    first comit

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+
+$function('.header__slider').slick({
+    infinity: true,
+    // fade: true, 
+    // prevArrow: '<img class="slick-arrow slick-arrow__prev" src="images/arrows-left.svg" alt=""></img>',
+    // nextArrow: '<img class="slick-arrow slick-arrow__next" src="images/arrows-right.svg" alt=""></img>',
+}) 
+  
+});
